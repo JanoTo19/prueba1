7c7a3dc1dc84515b97ec1fba3404fd2dd3d52dbd
JanoT19 <janotoralsanmiguel2004@gmail.com>
2024-04-16 09:59:26 +0200

    segundo dia prube commit

--- src/mypkt/Main.java
+++ src/mypkt/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		String nombre = "Jano";
+		String nombre = "Jano Toral";
 		
 		System.out.println("Hola " + nombre);
 		for (int i = 0; i < 10; i++) {
