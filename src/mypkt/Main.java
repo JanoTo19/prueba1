41e63e52a235b82393920da9e9ef090b3e8d3654
JanoT19 <janotoralsanmiguel2004@gmail.com>
2024-04-17 11:38:42 +0200

    prueba commit

--- src/mypkt/Main.java
+++ src/mypkt/Main.java
@@ -5,9 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		
 		final int LONGITUD = 10;
-		
+		System.out.println("Tabla del 2:");
 		for (int i = 0; i < LONGITUD; i++) {
-			System.out.println("Tabla del 2:\n" + 2 + " x " + (i+1) + " = " + (2*(i+1)));
+			System.out.println(2 + " x " + (i+1) + " = " + (2*(i+1)));
 		}
 		
 	}
